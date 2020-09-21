894cd69f5b7ab857889c5f57ccff6b9e19fdbf0b
ChrisH <68413782+superchrisho@users.noreply.github.com>
2020-09-21 21:22:12 +0800

    Update app.js
    toggle onclick slides li in and out using gsap

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 const toggle = document.querySelector(".hamburger");
-const links = document.querySelector(".navbar-links")
+const links = document.querySelector(".navbar-links");
 
 const tl = gsap.timeline().pause(true);
 
 tl.from(".links", {
   opacity: 0,
   duration: 0.5,
-  x: 300,
-  stagger: 0.2
-})
+  x: "100%",
+  stagger: 0.2,
+});
 
 toggle.addEventListener("click", () => {
-  links.classList.toggle("hide");
+  // links.classList.toggle("hide");
   tl.reversed() ? tl.play() : tl.reverse();
 });
