5c925139bc4cb85626a4eaefd04b33d50c9c8474
ChrisH <68413782+superchrisho@users.noreply.github.com>
2020-09-24 11:33:11 +0800

    animation hamburger

--- js/app.js
+++ js/app.js
@@ -1,32 +1,61 @@
 const toggle = document.querySelector(".hamburger");
 const links = document.querySelector(".navbar-links");
+const tl = gsap.timeline();
 
-const tl = gsap.timeline().pause(true);
-
+tl.pause();
 tl.from(".links", {
   opacity: 0,
-  duration: 0.5,
-  x: "100%",
+  x: "200%",
   stagger: 0.2,
 })
   .to(
     ".line1",
     {
-      transformOrigin: "center left",
-      rotate: "45deg",
+      top: "50%",
+      y: "-50%",
+      width: "0%",
+      duration: 0.2,
     },
     "<"
   )
-  .to(".line2", { width: "0px" }, "<")
+  .to(
+    ".line2",
+    {
+      width: "0%",
+      duration: 0.2,
+    },
+    "<"
+  )
   .to(
     ".line3",
     {
-      transformOrigin: "center left",
-      rotate: "-45deg",
+      top: "50%",
+      y: "-50%",
+      width: "0%",
+      duration: 0.2,
     },
     "<"
-  );
+  )
+  .to(".line1", {
+    width: "100%",
+    rotate: "45deg",
+    duration: 0.2,
+    backgroundColor: "lightcoral",
+  })
+  .to(
+    ".line3",
+    {
+      width: "100%",
+      rotate: "-45deg",
+      duration: 0.2,
+      backgroundColor: "lightcoral",
+    },
+    "<"
+  )
+  .reverse();
 
 toggle.addEventListener("click", () => {
+  console.log(`Played animation`);
+  tl.resume();
   tl.reversed() ? tl.play() : tl.reverse();
 });
